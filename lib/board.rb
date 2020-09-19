5efd3000aff0f9ca1c9d071b578d400b58296c04
isaacrez <isaacrez20@gmail.com>
2020-09-19 11:00:39 -0400

    Refactor board population to separate file

--- lib/board.rb
+++ lib/board.rb
@@ -1,37 +1,20 @@
 require './lib/board_display'
+require './lib/board_populate'
 require './lib/piece'
 
 class Board
   include BoardDisplay
+  include BoardPopulate
 
   @@SIZE = {x: 8, y: 8}
 
   def initialize
     @content = Array.new(@@SIZE[:x]) {Array.new(@@SIZE[:y]) {' '}}
-    populate_board
+    populate_board @content
     Displayer.display @content
   end
 
-  def populate_board
-    add_pawns
-    add_ranked
-  end
 
-  def add_pawns
-    0.upto 7 do |i|
-      @content[1][i] = Piece.new('pawn', :p1)
-      @content[6][i] = Piece.new('pawn', :p2)
-    end
-  end
-
-  def add_ranked
-    pieces = ['rook', 'knight', 'bishop', 'knight', 'queen', 'bishop', 'knight', 'rook']
-
-    0.upto 7 do |i|
-      @content[0][i] = Piece.new(pieces[i], :p1)
-      @content[7][i] = Piece.new(pieces[i], :p2)
-    end
-  end
 
 end
 
---
+++ lib/board_populate.rb
@@ -0,0 +1,39 @@
+module BoardPopulate
+
+  def populate_board(content)
+    populater = BoardPopulater.new(content)
+    return populater.content
+  end
+
+  class BoardPopulater
+    attr_reader :content
+
+    def initialize(content)
+      @content = content
+      populate_board
+    end
+
+    private
+    def populate_board
+      add_pawns
+      add_ranked
+    end
+
+    def add_pawns
+      0.upto 7 do |i|
+        @content[1][i] = Piece.new('pawn', :p1)
+        @content[6][i] = Piece.new('pawn', :p2)
+      end
+    end
+
+    def add_ranked
+      pieces = ['rook', 'knight', 'bishop', 'knight', 'queen', 'bishop', 'knight', 'rook']
+
+      0.upto 7 do |i|
+        @content[0][i] = Piece.new(pieces[i], :p1)
+        @content[7][i] = Piece.new(pieces[i], :p2)
+      end
+    end
+  end
+
+end
