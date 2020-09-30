87dcda40b6d68181e36324cf79317a767749e313
isaacrez <isaacrez20@gmail.com>
2020-09-30 14:47:57 -0400

    Refactor diagonal and horizontal mov't

--- lib/piece/movement.rb
+++ lib/piece/movement.rb
@@ -33,102 +33,119 @@ module MovementLogic
   end
 end
 
+module HorizontalMovement
+  @@HORZ_DIR = {UP: 0, RIGHT: 1, DOWN: 2, LEFT: 3}
+
+  def horizontal_move(board)
+    HorizontalMove.new(board, @position).get_moves
+  end
+
+  class HorizontalMove
+    include MovementLogic
+
+    def initialize(board, position)
+      @board = board
+      @position = position
+      @moves = []
+      config_iterators
+    end
+
+    def config_iterators
+      x, y = @position
+      @iter = {
+        up: (0...y).to_a.reverse,
+        right: ((x + 1)..8).to_a,
+        down: ((y + 1)..8).to_a,
+        left: (0...x).to_a.reverse
+      }
+    end
+
+    def get_moves
+      @iter.each_key do |dir|
+        get_moves_in dir
+      end
+      
+      return @moves
+    end
+
+    def get_moves_in(dir)
+      x, y = @position
+      iterator = @iter[dir]
+  
+      iterator.each do |i|
+        pos = is_vertical?(dir) ? [x, i] : [i, y]
+        valid_move?(pos, @board) ? @moves.append(pos) : break
+        @board.occupied?(pos) ? break : next
+      end
+    end
+
+    def is_vertical?(dir)
+      dir == :up || dir == :down
+    end
+  end
+end
+
+module DiagonalMovement
+  def diagonal_move(board)
+    DiagonalMove.new(board, @position).get_moves
+  end
+
+  class DiagonalMove
+    include MovementLogic
+
+    def initialize(board, position)
+      config_iterators(position)
+      @board = board
+      @moves = []
+    end
+
+    def config_iterators(position)
+      x, y = position
+
+      @x_iter = {
+        up_left: ((x + 1)..8).to_a,
+        up_right: ((x + 1)..8).to_a,
+        down_left: (0...x).to_a.reverse,
+        down_right: (0...x).to_a.reverse
+      }
+
+      @y_iter = {
+        up_left: ((y + 1)..8).to_a,
+        up_right: (0...y).to_a.reverse,
+        down_left: (0...y).to_a.reverse,
+        down_right: ((y + 1)..8).to_a
+      }
+    end
+
+    def get_moves
+      @x_iter.each_key do |dir|
+        get_moves_in dir
+      end
+      
+      return @moves
+    end
+
+    def get_moves_in(dir)
+      length = move_length(dir)
+      0.upto(length) do |i|
+        pos = [@x_iter[dir][i], @y_iter[dir][i]]
+        valid_move?(pos, @board) ? @moves.append(pos) : break
+        @board.occupied?(pos) ? break : next
+      end
+    end
+
+    def move_length(dir)
+      [@x_iter[dir].size, @y_iter[dir].size].min - 1
+    end
+  end
+end
+
 module DirectionalMovement
   include BoardConfig
-
-  @@HORZ_DIR = {UP: 0, RIGHT: 1, DOWN: 2, LEFT: 3}
+  include DiagonalMovement
+  include HorizontalMovement
 
   def omnidirectional_move(board)
     return diagonal_move(board).concat(horizontal_move(board))
-  end
-
-  def diagonal_move(board)
-    moves = []
-    x_i, y_i = @position
-
-    x_iter = ((x_i + 1)..8).to_a
-    y_iter = ((y_i + 1)..8).to_a
-    length = [x_iter.size, y_iter.size].min - 1
-
-    0.upto(length) do |i|
-      pos = [x_iter[i], y_iter[i]]
-      valid_move?(pos, board) ? moves.append(pos) : break
-      board.occupied?(pos) ? break : next
-    end
-
-    x_iter = ((x_i + 1)..8).to_a
-    y_iter = (0...y_i).to_a.reverse
-    length = [x_iter.size, y_iter.size].min - 1
-
-    0.upto(length) do |i|
-      pos = [x_iter[i], y_iter[i]]
-      valid_move?(pos, board) ? moves.append(pos) : break
-      board.occupied?(pos) ? break : next
-    end
-
-    x_iter = (0...x_i).to_a.reverse
-    y_iter = (0...y_i).to_a.reverse
-    length = [x_iter.size, y_iter.size].min - 1
-
-    0.upto(length) do |i|
-      pos = [x_iter[i], y_iter[i]]
-      valid_move?(pos, board) ? moves.append(pos) : break
-      board.occupied?(pos) ? break : next
-    end
-
-    x_iter = (0...x_i).to_a.reverse
-    y_iter = ((y_i + 1)..8).to_a
-    length = [x_iter.size, y_iter.size].min - 1
-
-    0.upto(length) do |i|
-      pos = [x_iter[i], y_iter[i]]
-      valid_move?(pos, board) ? moves.append(pos) : break
-      board.occupied?(pos) ? break : next
-    end
-
-    return moves
-  end
-
-  def horizontal_move(board)
-    moves = []
-    x_i, y_i = @position
-
-    @@HORZ_DIR.each_value do |dir|
-      moves.concat(add_horizontal(board, dir))
-    end
-
-    return moves
-  end
-
-  def add_horizontal(board, dir)
-    moves = []
-    x, y = @position
-    iterator = get_horz_iterator(dir)
-
-    iterator.each do |i|
-      pos = is_vertical?(dir) ? [x, i] : [i, y]
-      valid_move?(pos, board) ? moves.append(pos) : break
-      board.occupied?(pos) ? break : next
-    end
-
-    return moves
-  end
-
-  def is_vertical?(dir)
-    dir == @@HORZ_DIR[:UP] || dir == @@HORZ_DIR[:DOWN]
-  end
-
-  def get_horz_iterator(dir)
-    x, y = @position
-    case dir
-    when @@HORZ_DIR[:UP]
-      iterator = (0...y).to_a.reverse
-    when @@HORZ_DIR[:RIGHT]
-      iterator = ((x + 1)..8).to_a
-    when @@HORZ_DIR[:DOWN]
-      iterator = ((y + 1)..8).to_a
-    when @@HORZ_DIR[:LEFT]
-      iterator = (0...x).to_a.reverse
-    end
   end
 end
